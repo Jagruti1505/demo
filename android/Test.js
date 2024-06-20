53f3552c80f36e6b4893a8e762c45153be79d43d
Jagruti1505 <155343475+Jagruti1505@users.noreply.github.com>
2024-06-20 15:02:22 +0530

    <p>Test</p>

---
+++ android/Test.js
@@ -0,0 +1,11 @@
+
+const Util = require('../config/dataHandler.js');
+
+describe('<enter scenario name>', () => {
+
+    it('<Enter test case details>', async () => {
+
+        //Please paste the recorded script over here
+
+    });
+});
